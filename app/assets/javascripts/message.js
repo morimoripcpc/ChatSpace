43244d177f3780bcdc802442a2a6d298fe35248f
morimoripcpc <58966192+morimoripcpc@users.noreply.github.com>
2020-01-07 15:22:33 +0900

    yuuu

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,10 @@
 
 $(function(){ 
+
+  console.log(last_message_id);
+
   function buildHTML(message){
-   if ( message.image ) {
+    if (message.content && message.image) {
      var html =
       `<div class="message" data-message-id=${message.id}>
          <div class="upper-message">
@@ -20,6 +23,7 @@ $(function(){
          <img src=${message.image} >
        </div>`
      return html;
+
    } else {
      var html =
       `<div class="message" data-message-id=${message.id}>
@@ -44,25 +48,42 @@ $('#new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
  var url = $(this).attr('action')
+ last_message_id = $('.message:last').data("message-id");
+
  $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
+   url: "api/messages",
+   type: 'get',
    dataType: 'json',
+   data:  {id: last_message_id},
    processData: false,
    contentType: false
  })
-  .done(function(data){
-    var html = buildHTML(data);
+
+  .done(function(messages){
+    console.log('success');
+    if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+
+    var html = buildHTML(message);
     $('.main-content').append(html);
     $('.main-content').animate({ scrollTop: $('.main-content')[0].scrollHeight});
     $('form')[0].reset();
     $('.form__submit').prop('disabled', false);
+    }
+    $('.messages').append(insertHTML);
   })
+
   .fail(function() {
+    console.log('error');
     alert("メッセージ送信に失敗しました");
   });
 })
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
 
 
