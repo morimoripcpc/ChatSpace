b1fd411c86bb784ce9cab3cdba06793cba094b5d
morimoripcpc <58966192+morimoripcpc@users.noreply.github.com>
2020-01-08 15:49:38 +0900

    っっっf

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -71,7 +71,7 @@ $(function(){
 //  自動更新
     var reloadMessages = function() {
     last_message_id = $('.message:last').data("message-id");
-    console.log(last_message_id);
+    
     $.ajax({
       url: "api/messages",
       type: 'get',
@@ -81,7 +81,7 @@ $(function(){
     })
 
       .done(function(messages){
-        console.log(messages);
+        
         if (messages.length !== 0) {
           var insertHTML = '';
           messages.forEach(function(message) {
@@ -97,7 +97,7 @@ $(function(){
       })
 
       .fail(function() {
-        console.log('error');
+        
         alert("メッセージ送信に失敗しました");
       });
     };
