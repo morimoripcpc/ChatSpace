27d9c5407d098bd4bbc3b06359fd93dc76c5c1b3
morimoripcpc <58966192+morimoripcpc@users.noreply.github.com>
2020-01-09 22:03:19 +0900

    dfghjk

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,4 +46,11 @@ namespace :deploy do
 end
 
 
+set :default_env, {
+rbenv_root: "/usr/local/rbenv",
+path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+
 set :linked_files, %w{ config/secrets.yml }
